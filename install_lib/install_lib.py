930bea52c14a143d0173c3753798a34e5252645e
Luan Truong <truongluan303@gmail.com>
2022-01-30 00:39:18 -0800

    removed pip installation

--- install_lib/install_lib.py
+++ install_lib/install_lib.py
@@ -29,12 +29,9 @@ def install_required_libraries():
 
     print(">>> Installing Dependencies...\n\n")
 
-    # make sure pip is installed first
-    os.system(sys.executable + " " + os.getcwd() + "/install_lib/get_pip.py")
-    
     # install curses
     __install('windows-curses')
 
     # clear screen and inform that the process is finished
     __clear_screen()
-    print("--- FINISHED INSTALLATIONS ---")
+    print("--- INSTALLATIONS FINISHED ---")
